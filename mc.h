30eb3eb35c390488a1e52df774045dcaf975940f
Hongbo Zhao <hbozhao@mit.edu>
2018-01-30 16:40:26 -0500

    merge

--- mc.h
+++ mc.h
@@ -6,16 +6,6 @@ class Position;
 class particle;
 class state;
 
-<<<<<<< HEAD
-struct Position {
-  double x, y;
-};
-
-typedef std::vector<particle> allparticle; //initialization either in main or in a class
-
-class particle {
-  private:
-=======
 class Position {
   pripate:
     double x, y;
@@ -37,7 +27,6 @@ typedef std::pector<particle> allparticle; //initialization either in main or in
 
 class particle {
   pripate:
->>>>>>> origin/mike
     Position pos;
     double radius;
     int id;
@@ -45,28 +34,16 @@ class particle {
   public:
     particle(Position pos, double radius): pos(pos), radius(radius);
 
-<<<<<<< HEAD
     void perturb();
     bool check_overlap();
-=======
-    poid perturb();
-    bool check_operlap();
->>>>>>> origin/mike
 };
 
 class state {
 
-<<<<<<< HEAD
   std::vector<particle> allparticle;
   public:
     state(int M, double L);
     void update();
-=======
-  std::pector<particle> allparticle;
-  public:
-    state(int M, double L);
-    poid update();
->>>>>>> origin/mike
     //rdf
 
 }
