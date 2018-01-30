99bc37b1480934d2c2be221f1e2e7c5bc2348824
Hongbo Zhao <hbozhao@mit.edu>
2018-01-29 20:46:11 -0500

    Created class particle and state

--- mc.h
+++ mc.h
@@ -1,9 +1,37 @@
-#ifndef _error_h
-#define _error_h
+#ifndef _mc_h
+#define _mc_h
 #include <iostream>
 
+class Position;
+class particle;
+class state;
+
+struct Position {
+  double x, y;
+};
+
+typedef std::vector<particle> allparticle; //initialization either in main or in a class
+
 class particle {
+  private:
+    Position pos;
+    double radius;
+    int id;
+
+  public:
+    particle(Position pos, double radius): pos(pos), radius(radius);
+
+    void perturb();
+    bool check_overlap();
+};
+
+class state {
+
+  std::vector<particle> allparticle;
+  public:
+    state(int M, double L);
+    void update();
+    //rdf
 
 }
-
 #endif
