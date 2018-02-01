5094774eb1d5647c7ff5f18c02575486809b0259
Hongbo Zhao <hbozhao@mit.edu>
2018-01-31 19:29:53 -0500

    First rdf commit

--- mc.cpp
+++ mc.cpp
@@ -8,6 +8,8 @@
 #include <ctime>
 #include "mc.h"
 
+const double PI  =3.141592653589793238463;
+
 double Particle::distance2(Particle &p, double L) {
   double dx = abs(this->pos.x - p.pos.x);
   double dy = abs(this->pos.y - p.pos.y);
@@ -31,8 +33,14 @@ bool Particle::perturb(Position dv, State *s, double L){
   if (x_new > L) {
     x_new -= L;
   }
+  if (x_new < 0) {
+    x_new += L;
+  }
   if (y_new > L) {
     y_new -= L;
+  }
+  if (y_new < 0) {
+    y_new += L;
   }
   Position pos = {x_new, y_new};
   Particle candidate = Particle(pos, this->radius, this->id);
@@ -87,15 +95,46 @@ void State::update(int id, Position dv) {
 };
 
 std::ostream &operator<< (std::ostream & os, State &s) {
-  os << "Number of success = " << s.success << std::endl;
-  for (auto it = s.ap.begin(); it != s.ap.end(); ++it) {
-    os << *it;
+  os << "Success ratio = " << (double) s.success / s.attempt << std::endl;
+  // for (auto it = s.ap.begin(); it != s.ap.end(); ++it) {
+  //   os << *it;
+  // }
+}
+
+void State::cumrdf(std::vector<double> &prevRDF) {
+  double dist, binsize, maxdist, density;
+  int binnum, nbin, np;
+  nbin = prevRDF.size();
+  maxdist = sqrt(2)/2*L;
+  binsize = maxdist / nbin;
+  np = ap.size();
+  density = np/pow(L,2);
+  std::vector<double> hist(nbin, 0);
+  for (int i = 0; i < np; i++) {
+    for (int j = i+1; j < np; j++) {
+      dist = ap[i].distance(ap[j], L);
+      binnum = (int) floor(dist/binsize);
+      if (binnum == nbin) binnum--;
+      // std::cout << binnum;
+      hist[binnum] += 2;
+    }
   }
+  for (int i = 0; i < nbin; i++) {
+    prevRDF[i] += hist[i] / (np*(np-1)) / (M_PI*(pow((i+1)*binsize,2)-pow(i*binsize,2))*density);
+    // std::cout << prevRDF[i];
+  }
+}
+
+std::ostream &operator<< (std::ostream &os, std::vector<double> &v) {
+  for (int i = 0; i < v.size(); i++) {
+    os << v[i] << std::endl;
+  }
+  return os;
 }
 
 int main() {
   double L, radius, mag;
-  int M, Nsteps;
+  int M, Nsteps, nbin;
   Position dv;
 
   std::cout << "Enter box length" << std::endl;
@@ -104,6 +143,7 @@ int main() {
   std::cin >> M;
   std::cout << "Enter the particle radius" << std::endl;
   std::cin >> radius;
+  std::cout << "Volume fraction" << M_PI * pow(radius, 2) * pow((double) M/L, 2) << std::endl;
   std::cout << "Enter the perturbation magnitude" << std::endl;
   std::cin >> mag;
   std::cout << "Enter the number of steps" << std::endl;
@@ -113,11 +153,17 @@ int main() {
   // std::cout << "Original" << std::endl;
   // std::cout << state << std::endl;
 
+  std::cout << "Enter nbin" << std::endl;
+  std::cin >> nbin;
+  std::vector<double> RDF(nbin, 0);
+
   srand(1);
 
   std::clock_t time;
   double duration;
   time = std::clock();
+
+
 
   for (int i = 0; i < Nsteps; i++) {
     for (int j = 0; j < pow(M,2); j++) {
@@ -125,10 +171,19 @@ int main() {
       dv.y = (double) rand() / (RAND_MAX) * mag;
       state.update(j, dv);
     }
-    duration = ( std::clock() - time ) / (double) CLOCKS_PER_SEC;
-    time = std::clock();
-    std::cout << i << " takes " << duration << " s" << std::endl;
+    // duration = ( std::clock() - time ) / (double) CLOCKS_PER_SEC;
+    // time = std::clock();
+    // std::cout << i << " takes " << duration << " s" << std::endl;
+    state.cumrdf(RDF);
+    // std::cout << RDF << std::endl;
   }
+
+  for (int i = 0; i < nbin; i++) {
+    RDF[i] = RDF[i] / Nsteps;
+  }
+
   std::cout << "Final state" << std::endl;
+  std::cout << RDF << std::endl;
   std::cout << state << std::endl;
+
 }
